501c3f41c37ca6d23d9f56352c8bed5b9b8a1430
Taq Karim <taqqui.karim@intersection.com>
2019-12-01 23:16:21 -0500

    swap .out with out

--- js/main.js
+++ js/main.js
@@ -31,7 +31,11 @@ contentDom.addEventListener('click', e => {
         if (e.target.getAttribute('href').indexOf('#') == 0) return;
         if (e.target.getAttribute('href').indexOf('https://colab.research.google.com/github/mottaquikarim/pycontent') !== -1) {
             let href = e.target.getAttribute('href')
-            e.target.setAttribute('href', href.replace('mottaquikarim/pycontent', 'mottaquikarim/PYTH122'))
+            e.target
+                .setAttribute(
+                    'href',
+                    href.replace('mottaquikarim/pycontent', 'mottaquikarim/PYTH122').replace('.out', 'out')
+                )
         }
 		e.preventDefault();
 		window.open(e.target.getAttribute('href'))
